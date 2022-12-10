ca060cc0f0d7009750c123fa70888915a6ceaa05
Arielgithub101 <ariel454@gmail.com>
2022-12-11 00:32:18 +0200

    mochash

--- db2.py
+++ db2.py
@@ -2,4 +2,19 @@ import sqlite3
 
 conn = sqlite3.connect("users.sqlite")
 
+cursor = conn.cursor()
 
+sql_query = """CREATE TABLE users (
+id integer PRIMARY KEY,
+first_name NUT NULL
+last_name NUT NULL
+p_number NUT NULL
+location NUT NULL
+gender NUT NULL
+rel_status NUT NULL
+intersted_in NUT NULL
+hobbis NUT NULL
+friends NUT NULL
+)"""
+
+cursor.execute(sql_query)
--- run2.py
+++ run2.py
@@ -1,16 +1,22 @@
-from flask import Flask
-from flask import request
-from flask import jsonify
-from flask import abort
+from flask import Flask, request, jsonify, abort
+import sqlite3
 from typing import Dict
 
 app = Flask(__name__)
 
-def post_validation(data_obj:Dict):
-    for key,value in data_obj.items():
-        if key == 'friends' or value is None:
+
+def req_body_validation(data_obj: Dict):
+    for key, value in data_obj.items():
+        if key == 'friends' or len(value) == 0:
             return True
     return False
+def db_connction():
+    conn = None
+    try:
+        conn = sqlite3.connect("users.sqlite")
+    except ConnectionError as e:
+        print(e)
+    return conn
 
 
 books = [
@@ -54,7 +60,7 @@ def home():
 
 
 @app.route('/api/user/id', methods=['GET'])
-def api_display_id():
+def api_display_user():
     # Check if an ID was provided as part of the URL.
     # If ID is provided, assign it to a variable.
     # If no ID is provided, display an error in the browser.
@@ -73,13 +79,17 @@ def api_display_id():
         return f'User not found', 404
 
 
-
 @app.route('/api/user', methods=['POST'])
 def api_create_user():
-
-
+    conn = db_connction()
+    cursor = conn.cursor()
     if request.method == 'POST':
-        if post_validation(request.json):
+        # cursor = conn.execute("SELECT * FROM users")
+        # users = [
+        #     dict(id=row[0] for row in cursor.fetchall())
+        #
+        # ]
+        if req_body_validation(request.json):
             return 'bad req', 400
 
         user_name = request.json['first_name']
@@ -94,8 +104,9 @@ def api_create_user():
 
         validation_user_exits = p_number
         for book in books:
+            # שליפה יוזריפ\מספרי טלפון מהדיבי
             if book['p_number'] == validation_user_exits:
-                return 'user alredy in system',409
+                return 'user alredy in system', 409
 
         new_obj = {'id': id,
                    'first_name': user_name,
@@ -108,44 +119,84 @@ def api_create_user():
                    'hobbis': hobbis}
 
         books.append(new_obj)
-        return f'new user with id {new_obj["id"]} createdd!', 200
-
-    # if request.method == 'PUT':
-    #     for book in books:
-    #         if book['id'] == id:
-    #             book['first_name'] = request.json['first_name']
-    #             book['last_name'] = request.json['last_name']
-    #             book['p_number'] = request.json['p_number']
-    #             book['location'] = request.json['location']
-    #             book['gender'] = request.json['gender']
-    #             book['rel_status'] = request.json['rel_status']
-    #             book['intersted_in'] = request.json['intersted_in']
-    #             book['hobbis'] = request.json['hobbis']
-    #             book['friends'] = request.json['friends']
-    #
-    #
-    #             update_obj = {
-    #                 'id': id,
-    #                 'first_name': book['first_name'],
-    #                 'last_name':  book['last_name'],
-    #                 'p_number': book['p_number'],
-    #                 'location':  book['location'],
-    #                 'gender':  book['gender'] ,
-    #                 'rel_status':   book['rel_status'],
-    #                 'intersted_in': book['intersted_in'],
-    #                 'hobbis':book['hobbis'],
-    #                 'friends': book['friends']}
-    #             return jsonify(update_obj), 200
-    # if request.method == 'DELETE':
-    #     for book in books:
-    #         if book['id'] == id:
-    #             books.pop(books.index(book))
-    #             return jsonify(books)
-    #
+        return f'new user with id {new_obj["id"]} createdd!', 201
 
 
+@app.route('/api/user/id', methods=['DELETE'])
+def api_delete_user():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if request.method == 'DELETE':
+        if 'id' in request.args:
+            id = int(request.args['id'])
+        else:
+            return abort(404)
+        for book in books:
+            # חיבור לדיבי לבדיקה על יוזר למחיקה
+            if book['id'] == id:
+                books.pop(books.index(book))
+                return f'User with id {id} deleted', 200
+        return f'User not found', 404
 
-#
+
+@app.route('/api/users', methods=['DELETE'])
+def api_delete_all_user():
+    # Check if an ID was provided as part of the URL.
+    # If ID is provided, assign it to a variable.
+    # If no ID is provided, display an error in the browser.
+    if request.method == 'DELETE':
+        # חיבור לדיבי לבדיקה על יוזר למחיקה של כולם
+        books.clear()
+        return f'all users dleted', 200
+
+
+@app.route('/api/user/id', methods=['PATCH'])
+def api_update_user():
+    if request.method == 'PATCH':
+        if 'id' in request.args:
+            id_d = int(request.args['id'])
+        else:
+            return abort(404)
+
+        if req_body_validation(request.json):
+            return 'bad req', 400
+
+        for book in books:
+            if book['id'] == 1:
+                # חיבור לדידי לשליפת תונים
+
+                book['first_name'] = request.json['first_name']
+                book['last_name'] = request.json['last_name']
+                book['p_number'] = request.json['p_number']
+                book['location'] = request.json['location']
+                book['gender'] = request.json['gender']
+                book['rel_status'] = request.json['rel_status']
+                book['intersted_in'] = request.json['intersted_in']
+                book['hobbis'] = request.json['hobbis']
+                # book['friends'] = request.json['friends']
+
+                update_obj = {
+                    'id': id_d,
+                    'first_name': book['first_name'],
+                    'last_name': book['last_name'],
+                    'p_number': book['p_number'],
+                    'location': book['location'],
+                    'gender': book['gender'],
+                    'rel_status': book['rel_status'],
+                    'intersted_in': book['intersted_in'],
+                    'hobbis': book['hobbis'],
+                    'friends': book['friends']}
+                return jsonify(update_obj), 200
+        return f'User not found', 404
+
+
+# if request.method == 'DELETE':
+#     for book in books:
+#         if book['id'] == id:
+#             books.pop(books.index(book))
+#             return jsonify(books)
+
 # @app.route('/api/create/user', methods=['GET', 'POST'])
 # def api_create_user():
 #     if request.method == 'GET':
@@ -176,9 +227,16 @@ def api_create_user():
 #         return  f'new user with id {new_obj["id"]} createdd!',200
 
 
-@app.route('/api/delete/user/id', methods=['GET'])
+
+
+@app.route('/api/try/id/friend', methods=['GET'])
 def api_delete_id():
-    pass
+    if 'id' in request.args:
+        id = int(request.args['id'])
+        return f'this is work {id}'
+    else:
+        return 'not god',404
+
 
 
 @app.route('/api/deleteAll/user', methods=['GET'])
